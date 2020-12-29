485e333d224773d9fd73785df68b2fad07ee2a6a
botn365 <Krampus.sack.never@gmail.com>
2020-12-29 05:31:57 +0100

    ad a very basic main menu

--- src/screens/MainMenu.py
+++ src/screens/MainMenu.py
@@ -1,6 +1,16 @@
-from screens.BaseScreen import BaseScreen
+from src.screens.BaseScreen import BaseScreen
+from src.SubWindow import SubWindow
+from src.button.NormalButton import NormalButton
 
 
 class MainMenu(BaseScreen):
-    def __init__(self):
-        
+    def __init__(self, aParrent: SubWindow):
+        x,y,l,h = aParrent.getSize()
+        self._window = aParrent.newChildWindow(x,y,l,h)
+        self._parrent = aParrent
+        self._Buttons = []
+        self._Buttons.append(NormalButton(self._window,1,(10,10),80,40,"test"))
+
+    def onRender(self):
+        for button in self._Buttons:
+            button.draw()
