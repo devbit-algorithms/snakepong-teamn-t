6c92297746a10b3a7c440a3366287c29c6655379
botn365 <Krampus.sack.never@gmail.com>
2020-12-29 05:31:23 +0100

    add drawing and drawing option to normal

--- src/button/NormalButton.py
+++ src/button/NormalButton.py
@@ -1,7 +1,21 @@
 from src.button.BaseButton import BaseButton
+from src.SubWindow import TextModes
+from src.Util import Colors
+import pygame
 
 
 class NormalButton(BaseButton):
-    def __init__(self,aID,aPos,aL,aH,aText):
-        super().__init__(aID,aPos,aL,aH)
+    def __init__(self,aParrent,aID,aPos,aL,aH,aText,aFontSize=24,aDrawBackground=False,aColorText=Colors.white,aColorBorder=Colors.black):
+        super().__init__(aParrent,aID,aPos,aL,aH)
+        self._font = pygame.font.SysFont('ubuntu',aFontSize)
         self._text = aText
+        self._colorText = aColorText
+        self._drawBackground = aDrawBackground
+        self._colorBackground = aColorBorder
+        self._textPos = (aL >> 1,aH >> 1)
+
+    def draw(self):
+        if self._drawBackground:
+            self._window.drawBackGround(self._colorBackground)
+        self._window.drawBorder(self._colorText,1,)
+        self._window.drawText(self._text,self._font,self._colorText,self._textPos,TextModes.CENTER)
