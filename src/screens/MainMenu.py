d0be54dbd182fa8f4ee48602bf751b30a1d4ae88
botn365 <Krampus.sack.never@gmail.com>
2020-12-29 21:19:51 +0100

    add quit and start buttens

--- src/screens/MainMenu.py
+++ src/screens/MainMenu.py
@@ -1,6 +1,7 @@
 from src.screens.BaseScreen import BaseScreen
 from src.SubWindow import SubWindow
 from src.button.NormalButton import NormalButton
+from src.Util import calcuateOffset,CenterModes
 
 
 class MainMenu(BaseScreen):
@@ -9,7 +10,11 @@ class MainMenu(BaseScreen):
         self._window = aParrent.newChildWindow(x,y,l,h)
         self._parrent = aParrent
         self._Buttons = []
-        self._Buttons.append(NormalButton(self._window,1,(10,10),80,40,"test"))
+        xOffset = calcuateOffset(80,l,0,1,CenterModes.DOWN)
+        yOffset = calcuateOffset(40,h,0,2,CenterModes.DOWN)
+        self._Buttons.append(NormalButton(self._window,1,(xOffset,yOffset),80,40,"START"))
+        yOffset = calcuateOffset(40,h,1,2,CenterModes.DOWN)
+        self._Buttons.append(NormalButton(self._window, 2, (xOffset,yOffset), 80, 40, "QUIT"))
 
     def onRender(self):
         for button in self._Buttons:
