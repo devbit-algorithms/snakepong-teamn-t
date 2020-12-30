3b1858bbe062943ab9cabd57dbaae74458f03dfd
botn365 <Krampus.sack.never@gmail.com>
2020-12-30 22:10:11 +0100

    add colision to worm

--- src/Entitys/EntityWorm.py
+++ src/Entitys/EntityWorm.py
@@ -1,6 +1,7 @@
-from src.Entitys.BaseEntity import BaseEntity
+from src.Entitys.BaseEntity import BaseEntity, HitBox, HitBoxSquare, HitBoxCircle
 from src.SubWindow import SubWindow
 from src.Utils.Util import Colors
+from src.Utils.ColisionUtils import colideCircleSquair, colideSquairs
 
 
 class Directions:
@@ -27,6 +28,9 @@ class EntityWorm(BaseEntity):
 
 class EntityWormBody(BaseEntity):
     def __init__(self,aParrent: SubWindow,aX,aY,aL,aH,aColorBody=Colors.white):
+        super().__init__(aX,aY)
+        self._l = aL
+        self._h = aH
         self._parrent = aParrent
         self._window = aParrent.newChildWindow(aX,aY,aL,aH)
         self._collisionWalls = []
@@ -41,12 +45,12 @@ class EntityWormBody(BaseEntity):
     def setNewPos(self,aPos):
         pass
 
-
-class EntityColisionWall(BaseEntity):
-    def __init__(self,aX,aY,aL,aH):
-        self._x = aX
-        self._y = aY
-        self._l = aL
-        self._h = aH
-
-    def colide(self,aEntity):
+    def colide(self,aHitBox):
+        hetBoxSelf = HitBoxSquare(self._x, self._y, self._l, self._h)
+        type = aHitBox.type
+        if type == HitBox.SQUAIR:
+            return 270 if colideSquairs(aHitBox,hetBoxSelf) else -1
+        elif type == HitBox.CIRCLE:
+            return 270 if colideCircleSquair(aHitBox,hetBoxSelf) else -1
+        else:
+            return -1
