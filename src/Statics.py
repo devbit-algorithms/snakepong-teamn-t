f6b11fb2f5280ec4e68f4e333cfc572d155c65d4
botn365 <Krampus.sack.never@gmail.com>
2020-12-30 01:49:59 +0100

    add a static class to get around circuliar deps

---
+++ src/Statics.py
@@ -0,0 +1,9 @@
+class Input:
+    mousEvent = None
+    keyEvent = None
+
+    # BUTTON_LEFT = 1
+    # BUTTON_MIDDLE = 2
+    # BUTTON_RIGHT = 3
+    # BUTTON_WHEELDOWN = 5
+    # BUTTON_WHEELUP = 4
