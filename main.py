d4268ba234ec674fef4c8ea7e636b3a4933358f1
botn365 <Krampus.sack.never@gmail.com>
2020-12-30 01:49:32 +0100

    allow main loop to be closed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import time
 from src.SubWindow import SubWindow
 from src.screens.MainMenu import MainMenu
+from src.Statics import Input
+from src.screens import BaseScreen
 
 
 class Game:
@@ -12,34 +14,53 @@ class Game:
         self.size = self.weight, self.height = 640, 400
         self._headDisplay = pygame.display.set_mode(self.size, pygame.SCALED)
         self._mainSubWindow = SubWindow(self._headDisplay, 0, 0, 640, 400)
-        self._window = MainMenu(self._mainSubWindow)
+        self._window: BaseScreen = MainMenu(self._mainSubWindow)
 
     def onEvent(self, event):
         if event.type == pygame.QUIT:
             self._running = False
-        elif event.type == pygame.MOUSEBUTTONUP:
-            a = event.pos[0]
-            b = event.pos[1]
+        elif event.type == pygame.MOUSEBUTTONUP or event.type == pygame.MOUSEBUTTONDOWN:
+            Input.mousEvent = event
 
     def onLoop(self):
+        timeA = time.time()
         self._window.onUpdate()
+        timeB = time.time()
+        # print(timeB-timeA," logic time")
+        Input.mousEvent = None
+        Input.keyEvent = None
 
     def onRender(self):
+        timeA = time.time()
         self._window.onRender()
+        timeB = time.time()
+        # print(timeB - timeA," render time")
         pygame.display.update()
 
     def onCleanup(self):
         pygame.quit()
 
+    def checkAlive(self):
+        if not self._window.isAlive():
+            self._window = self._window.onKill()
+            if self._window is None:
+                self._running = False
+        return self._running
+
 
 def main():
     game = Game()
-    while game._running:
+    while game.checkAlive():
+        timeA = time.time()
         for event in pygame.event.get():
             game.onEvent(event)
         game.onLoop()
         game.onRender()
-        time.sleep(0.16)
+        timeB = time.time()
+        timeTotal = timeB - timeA
+        sleepTime = 0.016 - timeTotal
+        if sleepTime > 0:
+            time.sleep(sleepTime)
     game.onCleanup()
 
 
