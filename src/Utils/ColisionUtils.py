e14b83c780dcc26e39ee2c69f7686edca76a4a02
botn365 <Krampus.sack.never@gmail.com>
2020-12-30 04:44:25 +0100

    add colision utols and enum

---
+++ src/Utils/ColisionUtils.py
@@ -0,0 +1,17 @@
+from src.Entitys.BaseEntity import BaseEntity
+
+class ColideUtils:
+    @staticmethod
+    def colideSquairs(aEntity1: BaseEntity, aEntity2):
+        pass
+
+    @staticmethod
+    def colideCircils(aEntity1: BaseEntity, aEnttiy2: BaseEntity):
+        pass
+
+    @staticmethod
+    def colideCircleSquair(aCircle: BaseEntity, aSquair: BaseEntity):
+        pass
+
+    SQUAIR = 0
+    CIRCLE = 1
