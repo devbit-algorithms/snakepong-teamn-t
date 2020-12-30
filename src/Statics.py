186fe9d4d47572f10a109adf45235bb04f9d5150
botn365 <Krampus.sack.never@gmail.com>
2020-12-30 04:25:01 +0100

    add Wolrd statis

--- src/Statics.py
+++ src/Statics.py
@@ -2,6 +2,18 @@ class Input:
     mousEvent = None
     keyEvent = None
 
+
+class World:
+    livingEntityList = []
+
+    @staticmethod
+    def addEnttity(aEntity):
+        World.livingEntityList.append(aEntity)
+
+    @staticmethod
+    def removeEnttiy(aEntity):
+        pass
+
     # BUTTON_LEFT = 1
     # BUTTON_MIDDLE = 2
     # BUTTON_RIGHT = 3
